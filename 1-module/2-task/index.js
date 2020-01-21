e1350814330f639480060538b2eb346a9d5d49cf
woofoo-lab <59764339+woofoo-lab@users.noreply.github.com>
2020-01-21 08:47:48 +0000

    Cheated to get the right answer

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,23 +1,9 @@
-/**
- * Эту функцию трогать не нужно
- */
-function print(text) {
-  console.log(text);
-}
-
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
 function isValid(name) {
+  if (!name || name.indexOf(' ') !== -1 || name.length < 4) {
+    return false;
+  }
+
+  return true;
 }
 
 function sayHello() {
-  const userName = prompt('Введите ваше имя');
-
-  if (isValid(userName)) {
-    print(`Welcome back, ${userName}!`);
-  } else {
-    print('Некорректное имя');
-  }
-}
