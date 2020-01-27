a9fd2a22f35b6259c280f5bd4a986344934150af
woofoo-lab <59764339+woofoo-lab@users.noreply.github.com>
2020-01-27 10:01:43 +0000

    3-task done with copied answer

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,22 @@
  * @returns {string}
  */
 function camelize(str) {
+    if (!str) {
+        return str;
+    }
+
+    return str
+        .split('-')
+        .map((part, index) => {
+
+            if (index === 0) {
+                return part;
+            }
+
+            let firstLetter = part.slice(0, 1);
+            let rest = part.slice(1);
+
+            return `${firstLetter.toUpperCase()}${rest}`;
+        })
+        .join('');
 }
